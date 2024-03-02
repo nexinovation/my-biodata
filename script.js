c14c5a404eb1a3d9a81bbdd6610ef4f97b04fe8a
senator olorunnisola <senatoreniola@gmail.com>
2024-03-02 13:07:42 +0100

    corrected a spelling

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("changeColorBtn").addEventListener('click', function() {
+var header = document.querySelector("header");
+var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+header.style.backgroundColor = randomColor;
