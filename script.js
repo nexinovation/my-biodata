934b7f5bbd58770e177fe16331c86a5d8eba1ede
senator olorunnisola <senatoreniola@gmail.com>
2024-03-02 13:13:35 +0100

    added a javascrip file

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById("changeColorBtn").addEventListener('click', function() {
-var header = document.querySelector("header");
-var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-header.style.backgroundColor = randomColor;
+    var header = document.querySelector("header");
+    var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    header.style.backgroundColor = randomColor;
+});
